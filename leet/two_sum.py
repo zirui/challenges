3410fdd732ab73c00c50d367540ff9669262341e
zirui-zhang <zirui-zhang@yiducloud.cn>
2015-08-22 09:37:31 +0800

    add two sum problem

---
+++ leet/two_sum.py
@@ -0,0 +1,27 @@
+"""
+Description:
+Given an array of integers, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+"""
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        map = {}
+        for i in range(len(nums)):
+            map[nums[i]] = i
+        for i in range(len(nums)):
+            m = target - nums[i]
+            if m in map and map[m] != i:
+                return i + 1, map[m] + 1
